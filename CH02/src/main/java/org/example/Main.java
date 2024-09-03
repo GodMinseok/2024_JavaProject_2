f45b95fb849e876f4d482af3c8cec1c663eaaced
GodMinseok <mseok830@naver.com>
2024-09-03 16:09:56 +0900

    엔티티매니저 팩토리, 매니저 생성 및 트랜잭션 획득

--- CH02/src/main/java/org/example/Main.java
+++ CH02/src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Hello world!");
+        // 엔티티 매니저 팩토리 생성
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa");
+        // 엔티티 매니저 생성
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        // 트랜잭션 획득
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+
+            transaction.begin();
+
+            // 로직 생성
+            logic(entityManager);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
         }
+        entityManagerFactory.close();
     }
+
+        private static void logic(EntityManager em){
+            String id = "test2";
+            Member member = new Member();
+            member.setId(id);
+            member.setUsername("testName2");
+            member.setAge(2);
+
+            // 등록
+            em.persist(member);
+
+            member.setAge(20);
+
+            Member findMember = em.find(Member.class, id);
+            System.out.println("findMember = " + findMember.getUsername() +"/ age = " + findMember.getAge());
+
+            List<Member> members = em.createQuery("select m from Member m").getResultList();
+            System.out.println("Members.size()="+members.size());
+
+        }
 }
