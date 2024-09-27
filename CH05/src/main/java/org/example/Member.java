075856af030364abbca4eacbcdfe281cf3ba2df1
GodMinseok <mseok830@naver.com>
2024-09-27 15:16:27 +0900

    CH05 수정

--- CH05/src/main/java/org/example/Member.java
+++ CH05/src/main/java/org/example/Member.java
@@ -2,11 +2,9 @@ package org.example;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import org.hibernate.annotations.Comment;
 
 @Entity
 public class Member {
